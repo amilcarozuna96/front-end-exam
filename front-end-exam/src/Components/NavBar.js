1876a4fa0f7f7e19a865da2e8847b97990d5af26
amilcarozuna96 <115510635+amilcarozuna96@users.noreply.github.com>
2022-11-03 16:48:27 -0600

    Add files via upload

---
+++ front-end-exam/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import classes from './NavBar.module.css'
+
+function NavBar () {
+    <div>
+        <nav>
+            <button>Users Page</button>
+            <button>Items Page</button>
+        </nav>
+    </div>
+}
+
+export default NavBar;
